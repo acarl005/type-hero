ba25f40ea1d96d3c15d67bd400dc61efdbf51422
Andrew Carlson <acarl005@g.ucla.edu>
2017-07-10 13:25:22 -0700

    add comments and better todo list

--- index.js
+++ index.js
@@ -4,8 +4,10 @@ const chalk = require('chalk')
 const path = require('path')
 const { highlight } = require('cli-highlight')
 
-const DEBUG = false
+// a runtime option for truning on some helpful internal info
+const DEBUG = true
 
+// create the one and only screen we'll need
 const screen = blessed.screen({
   cursor: {
     artificial: true,
@@ -14,10 +16,17 @@ const screen = blessed.screen({
   }
 })
 
+
+// these are all the keys that the player pressed
 const keyStack = []
+
+// load the code they are gonna type
 const code = fs.readFileSync(path.join(__dirname, 'index.js'), 'utf8')
 
+// this box will show the main game window
 const box = blessed.box({
+  parent: screen,
+  // give is the code and highlight the first character green!
   content: chalk.bgGreen(code[0]) + code.slice(1),
   height: DEBUG ? '50%' : '100%',
   scrollable: true,
@@ -26,37 +35,45 @@ const box = blessed.box({
   }
 })
 
+// use this to display the keystack in DEBUG mode
 const debug = blessed.box({
   top: '50%',
-  height: '50%-2',
+  height: '50%-1',
   border: {
     type: 'line'
   }
 })
 
+// this will be for arbitrary messages in DEBUG mode
 const alert = blessed.box({
-  top: '100%-2',
+  top: '100%-1',
   height: 2
 })
 
+// the message for passing to the alert box
 let alertMsg = ''
 
-screen.append(box)
 if (DEBUG) {
   screen.append(debug)
   screen.append(alert)
 }
 
+// the initial render!
 screen.render()
+
+// give us an out
 screen.key([ 'C-c' ], () => process.exit(0))
+
+// the handler for all other keypresses
 screen.on('keypress', (ch, key) => {
 
   // add some debug info
-  alertMsg += JSON.stringify(key)
+  alertMsg = JSON.stringify(key)
 
-  // handle the keypress
+  // ctrl+r is for removing the alert message in DEBUG mode
   if (key.ctrl && key.name === 'r') {
     alertMsg = ''
+  // on mac, pressing enter triggers TWO keypresses, one called 'enter' and one called 'return'. we only want to respond to one of those. so lets ignore 'return'
   } else if (
     key.name === 'return' ||
     key.ctrl
@@ -64,6 +81,7 @@ screen.on('keypress', (ch, key) => {
     'ignore'
   } else if (key.name === 'backspace') {
     keyStack.pop()
+  // handle all the other typing
   } else {
     const keyObj = {
       key: ch,
@@ -75,19 +93,23 @@ screen.on('keypress', (ch, key) => {
     keyStack.push(keyObj)
   }
 
-  // fix the scroll position
+  // fix the vertical scroll position, so that the screen moves downward as the game advances
   const lineNum = (code.slice(0, keyStack.length).match(/\n/g) || []).length
-  const innerBoxHeight = box.height - 2 // need to subtract the borders
+  // need to subtract the borders, which take up a width of 1 each
+  const innerBoxHeight = box.height - 2
+  // lets try to keep things centered
   const boxCenter = Math.floor(innerBoxHeight / 2)
   box.scrollTo(lineNum + boxCenter)
 
-  // highlight based on the current state
+  // highlight based on the current state, have they got everything correct up to this point?
   const correct = keyStack.every(keyObj => !keyObj.error)
   let highlighted
+  // if so, the "cursor" is green
   if (correct) {
     highlighted = highlight(code.slice(0, keyStack.length), { language: 'javascript' })
       + chalk.bgGreen(wrapNewlines(code[keyStack.length]))
       + code.slice(keyStack.length + 1)
+  // if not, the cursor is yellow and all the characters since the first error are red
   } else {
     const errIndex = keyStack.findIndex(keyObj => keyObj.error)
     highlighted = highlight(code.slice(0, errIndex), { language: 'javascript' })
@@ -96,13 +118,15 @@ screen.on('keypress', (ch, key) => {
       + code.slice(keyStack.length + 1)
   }
 
-  // update and render
+  // update and re-render everything
   box.setContent(highlighted)
   debug.setContent(keyStack.map(obj => obj.key).join(''))
   alert.setContent(String(alertMsg))
   screen.render()
 })
 
+
+// newlines are invlisible. lets give them a little arrow emoji to look at instead
 function wrapNewlines(ch) {
   if (ch === '\n') {
     return '↩︎ \n'
